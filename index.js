a5916c489bfed468d10d1fccf57398a990eaf2da
东灯 <43312495+Lampese@users.noreply.github.com>
2021-08-14 01:32:40 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 var WebSocket=require('ws');
-var Events={};
-exports.StartNeutron=function(){
-    var Ws=new WebSocket("ws://localhost:16384");
+var Events={},Ws;
+exports.StartNeutron=function(callback){
+    Ws=new WebSocket("ws://localhost:16384");
+    Ws.on("open",()=>{
+        var Listener=function(Event,callback){
+            Events[Event]=callback;
+            Ws.send(`{"Event":"sub","EventName":"${Event}"}`);
+        }
+        var CancelListener=function(Event){
+            if(Events[Event]!=undefined){
+                delete Events[Event];
+                Ws.send(`{"Event":"can","EventName":"${Event}"}`);
+            }else throw new Error("Cancel unsub event."); 
+        }
+        var Game={
+            ExecCommand:function(Command){
+                Ws.send(`{"Event":"Game","EventName":"ExecCommand","Command":"${Command}"}`);
+            }
+        }
+        callback(Listener,CancelListener,Game);
+    });
     Ws.on("message",(message)=>{
         let Message=JSON.parse(message.toString());
         Events[Message["Type"]](Message["Data"]);
     });
 };
-exports.Listener=function(Event,callback){
-    Events[Event]=callback;
-    Ws.send(`{"Event":"sub","EventName":${Event}}`);
-}
-exports.CancelListener=function(Event){
-    delete Events[Event];
-    Ws.send(`{"Event":"can","EventName":${Event}}`);
-}
-exports.Game={
-    ExecCommand:function(Command){
-        Ws.send(`{"Event":"Game","EventName":"ExecCommand","Command":${Command}}`);
-    }
-}
